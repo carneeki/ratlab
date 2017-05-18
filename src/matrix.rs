d70d90553b599309f833b0f7f44adb4dee0bb304
Adam Carmichael <carneeki@carneeki.net>
2017-05-19 07:16:24 +1000

    initial commit

---
+++ src/matrix.rs
@@ -0,0 +1,80 @@
+pub struct Matrix<T> {
+  _cols: u32,
+  _rows: u32,
+  _data: Vec<T>,
+}
+
+impl<T> Matrix<T> {
+    pub fn new(cols: u32, rows: u32) -> Matrix<T> {
+        Matrix {
+            _cols: cols,
+            _rows: rows,
+            _data: Vec::new(),
+        }
+    }
+
+    pub fn cols(&mut self) -> u32 { self._cols.clone() }
+    pub fn rows(&mut self) -> u32 { self._rows.clone() }
+
+    pub fn get<T>(&mut self, i: u32, j: u32) -> T {
+        self._data[(i+j*self.cols()) as usize]
+    }
+    pub fn set<T>(&mut self, i: u32, j: u32, d: T) {
+        self._data[(i+j*self.cols()) as usize] = d;
+    }
+
+    pub fn fill<T>(&mut self, v: T) -> &mut Matrix<T> {
+        self._data = vec![v, (self.cols()*self.rows()) as T];
+    }
+    pub fn identity(&mut self) -> &mut Matrix<T> {
+        if self.rows() != self.cols() {
+            panic!("Matrix is not square. Bye");
+        }
+
+        self.fill(0);
+        //self._data = vec![0 as f64, (self.cols()*self.rows()) as f64];
+
+        for i in 0..self.cols() {
+            self.set(i, i, 1);
+        }
+        /*
+        let mut i = 0;
+        while i < self._data.len()
+        {
+            self.data[i] = 1 as f64;
+            i+= (self.cols() +1) as usize;
+        }
+        */
+        self
+    }
+
+    pub fn finalize(&mut self) -> Matrix<T> {
+        Matrix {
+            _cols: self.cols(),
+            _rows: self.rows(),
+            _data: self._data.clone()
+        }
+    }
+}
+
+trait Squareness {
+    fn is_square(&self) -> bool;
+}
+
+impl Squareness for Matrix<T> {
+    fn is_square(&self) -> bool {
+        self.cols() == self.rows()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Matrix;
+
+    #[test]
+    fn zero_1x1() {
+        let mut m = Matrix::<f64>::new(1,1).zeroes();
+        assert_eq!(m.data.len(), 0);
+        assert_eq!(m.data[0], 0);
+    }
+}

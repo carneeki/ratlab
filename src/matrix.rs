e981d57f061ee2bb119566728a023b5e7a2879ad
Adam Carmichael <carneeki@carneeki.net>
2017-05-20 13:41:51 +1000

    interim

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,23 +1,27 @@
+use num_traits::Num;
+use std::ops::Index;
+use std::ops::IndexMut;
+
 #[derive(Debug)]
 struct Matrix<T> {
   _cols: usize,
   _rows: usize,
-  data: Vec<T>,
+  _data: Vec<T>,
 }
 
-impl<T: Num> Matrix<T> {
+impl<T: Num + Copy> Matrix<T> {
     pub fn new(cols: usize, rows: usize) -> Matrix<T> {
         Matrix {
             _cols: cols,
             _rows: rows,
-            data: Vec::<T>,
+            _data: Vec::new(),
         }
     }
 
     pub fn cols(&self) -> usize { self._cols }
     pub fn rows(&self) -> usize { self._rows }
 
-    pub fn fill_with(&mut self, v: <T>) -> &mut Matrix<T> {
+    pub fn fill_with(&mut self, v: T) -> &mut Matrix<T> {
         self._data = vec![v; (self.cols()*self.rows()) as usize];
         self
     }
@@ -31,17 +35,8 @@ impl<T: Num> Matrix<T> {
         let mut addr = 0;
         for i in 0..self.cols() {
             addr = (i + i*self.cols()) as usize;
-            self.data[addr] = 1;
-            //self.set(i, i, 1);
+            self._data[addr] = 1;
         }
-        /*
-        let mut i = 0;
-        while i < self._data.len()
-        {
-            self.data[i] = 1 as f64;
-            i+= (self.cols() +1) as usize;
-        }
-        */
         self
     }
 
@@ -54,17 +49,16 @@ impl<T: Num> Matrix<T> {
     }
 }
 
-impl Index<(usize,usize)> for Matrix<T> {
+impl<T : Num + Copy> Index<(usize,usize)> for Matrix<T> {
     type Output = T;
-    fn index(&self, idx: (usize,usize)) -> &<T> {
+    fn index(&self, idx: (usize,usize)) -> &T {
         let addr = (idx.0 + idx.1*self.cols()) as usize;
         self._data[addr]
     }
 }
 
-impl Index_Mut<(usize,usize)> for Matrix<T> {
-    type Output = T;
-    fn index(&mut self, idx: (usize,usize)) -> &mut <T> {
+impl<T : Num + Copy> IndexMut<(usize,usize)> for Matrix<T> {
+    fn index_mut(&mut self, idx: (usize,usize)) -> &mut T {
         let addr = (idx.0 + idx.1*self.cols()) as usize;
         self._data[addr]
     }
@@ -86,6 +80,7 @@ mod tests {
         let m = Matrix::default()
             .cols(3)
             .rows(3)
+            .fill_with(0)
             .build();
 
         println!("{:?}", m);

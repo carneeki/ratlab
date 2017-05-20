5f9ca16c10fbed1dcbc9f838466f44fe539c18f3
Adam Carmichael <carneeki@carneeki.net>
2017-05-21 09:43:14 +1000

    interim commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+//! Matrices in Rust (sort of)
+//!
+//! # Example Types
+//!
+//! * Row vectors
+//! * Column vectors
+//! * 2x2, 3x3, to mxn matrices
+//!
+//! Matrices are stored internally as row-major order as a vector, however
+//! they are addressed using column-major order by default (that is M_rc).
+//! The 'getter' is to access the vector by way of a tuple. Suppose we have
+//! the matrix `M` =
+//! ```math
+//! ┌      ┐
+//! │ 4  3 │
+//! │ 2  1 │
+//! └      ┘
+//! ```
+//! The element '3' would be accessed by `M[(0,1)]`, and we could write a new
+//! value to the position occupied by 2 with `M[(1,0)] = 0;`
+//!
+//! A MatrixBuilder has been provided to aid in building matrices which uses
+//! `.set_cols(usize)` and `.set_rows(usize)` to set the dimensions.
+//!
+//! ## TODO List
+//! The Matrix implementation will include functions for accessing row and
+//! column vectors such that
+//! `M` =
+//! ```math
+//! ┌      ┐
+//! │ 4  3 │
+//! │ 2  1 │
+//! └      ┘
+//! ```
+//! could be accessed by way of `M.col(0)` would return a column vector:
+//! ```math
+//! ┌   ┐
+//! │ 4 │
+//! │ 2 │
+//! └   ┘
+//! ```
+//! or `M.row(1)` would return a row vector:
+//! ```math
+//! ┌      ┐
+//! │ 4  3 │
+//! └      ┘
+//! ```
+//! or the two could be combined to access a single element, `m.col(1).row(1)`
+//! ```math
+//! ┌   ┐
+//! │ 1 │
+//! └   ┘
+//! ```
+//! A further refinement might be to take a count of the elements returned
+//! prior to actually returning them, and if only a single value is returned,
+//! return it as a single value rather than as a vector. This will require some
+//! consideration to see if it is worthwhile. Methods such as
+//! `M.get_rc<T>(row,col)` and `M.get_cr<T>(col,row)` might be more sensible.
+//! concerning return types and ordering
+
 extern crate num;
 extern crate num_traits;
 
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,10 @@
 use num_traits::Num;
 use std::ops::{Index,IndexMut};
 
+/// An r x c matrix of a numeric type.
+///
+/// # Examples
+/// `TODO: write me`
 #[derive(Debug)]
 struct Matrix<T> {
   _cols: usize,
@@ -8,15 +12,42 @@ struct Matrix<T> {
   _data: Vec<T>,
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Inherent methods
+////////////////////////////////////////////////////////////////////////////////
+
 impl<T: Num + Copy> Matrix<T> {
+    /// Constructs a new, zero filled r x c matrix
+    ///
+    /// # Example:
+    ///
+    /// ```norun
+    /// let mut m = Matrix::new(1,3);
+    /// ```
+    /// Should return a single row matrix, `m == (0  0  0)`
+    pub fn new(rows: usize, cols: usize) -> Matrix<T> {
+        Matrix{
+            _cols: cols,
+            _rows: rows,
+            _data: Vec::new(),
+        }
+    }
+    /// Gets the number of columns in matrix
     pub fn cols(&self) -> usize { self._cols }
+
+    /// Gets the number of rows in matrix
     pub fn rows(&self) -> usize { self._rows }
 
+    /// Fills matrix with a given number
     pub fn fill_with(&mut self, v: T) -> &mut Matrix<T> {
         self._data = vec![v; (self.cols() * self.rows()) as usize];
         self
     }
 
+    /// Converts the current matrix into an identity matrix.
+    ///
+    /// # Note:
+    /// Matrix *must* be a square matrix or a panic will occur.
     pub fn identity(&mut self) -> &mut Matrix<T> {
         if self.is_square() == false {
             panic!("Identity matrices must be square.");
@@ -40,53 +71,77 @@ impl<T : Num + Copy> Square for Matrix<T> {
 
 impl<T : Num + Copy> Index<(usize,usize)> for Matrix<T> {
     type Output = T;
+    /// Used to get element rc from a matrix where r is row, c is col
+    /// with a zero based index.
+    /// # Example:
+    /// Given the following matrix:
+    /// ```text
+    ///     ( 1   1   2 )
+    /// m = ( 9   9   9 )
+    ///     ( 3   4   3 )
+    /// ```
+    /// `m[(1,3)]` will return 2
     fn index(&self, idx: (usize,usize)) -> &T {
-        let addr = (idx.0 + idx.1*self.cols()) as usize;
+        let addr = (idx.0*self.cols() + idx.1) as usize;
         &self._data[addr]
     }
 }
 
 impl<T : Num + Copy> IndexMut<(usize,usize)> for Matrix<T> {
+    /// Used to get element rc from a matrix where r is row, c is col
+    /// with a zero based index.
+    /// # Example:
+    /// Given the following matrix:
+    /// ```text
+    ///     ( 1   1   2 )
+    /// m = ( 9   9   9 )
+    ///     ( 3   4   3 )
+    /// ```
+    /// `m[(1,3)] = 4` will set the 2 to a 4
     fn index_mut(&mut self, idx: (usize,usize)) -> &mut T {
-        let addr = (idx.0 + idx.1*self.cols()) as usize;
+        let addr = (idx.0*self.cols() + idx.1) as usize;
         &mut self._data[addr]
     }
 }
 
 #[derive(Debug)]
-struct MatrixBuilder<T>(Matrix<T>);
+struct MatrixBuilder<T> {
+  _cols: usize,
+  _rows: usize,
+  _data: Vec<T>,
+}
 
 impl<T: Num + Copy> MatrixBuilder<T> {
-    pub fn new() -> Matrix<T> {
-        Matrix {
+    pub fn new() -> MatrixBuilder<T> {
+        MatrixBuilder {
             _cols: 0 as usize,
             _rows: 0 as usize,
             _data: Vec::new(),
         }
     }
 
-    pub fn set_cols(&mut self, cols: usize) -> &mut Matrix<T> {
-        (self.0)._cols = cols;
-        &mut self.0
+    pub fn set_cols(&mut self, cols: usize) -> &mut MatrixBuilder<T> {
+        self._cols = cols;
+        self
     }
 
-    pub fn set_rows(&mut self, rows: usize) -> &mut Matrix<T> {
-        (self.0)._rows = rows;
-        &mut self.0
+    pub fn set_rows(&mut self, rows: usize) -> &mut MatrixBuilder<T> {
+        self._rows = rows;
+        self
     }
 
-    pub fn finalize(self) -> Matrix<T> { // takeownership and destroy self
-        if (self.0)._cols == 0 {
+    pub fn finalize(&self) -> Matrix<T> {
+        if self._cols == 0 {
             panic!("cols cannot be zero");
         }
-        if (self.0)._rows == 0 {
+        if self._rows == 0 {
             panic!("rows cannot be zero");
         }
 
         Matrix {
-            _cols: (self.0).cols(),
-            _rows: (self.0).rows(),
-            _data: (self.0)._data,
+            _cols: self._cols,
+            _rows: self._rows,
+            _data: vec![T::zero(); (self._rows*self._cols)]
         }
     }
 }
@@ -97,10 +152,23 @@ mod tests {
 
     #[test]
     fn zero_1x1() {
-        let mut m = MatrixBuilder::new().set_cols(1).set_rows(1).will_with(0).finalize();
+        let mut m = MatrixBuilder::new()
+            .set_cols(1)
+            .set_rows(1)
+            .finalize();
+
+        m._cols = 900;  // FIXME: This should have errored during compile
 
         assert_eq!(m.cols()  , 1 as usize);
         assert_eq!(m.rows()  , 1 as usize);
+
+        m._data[0] = 1; // FIXME: This should have errored during compile
+                        // Should not be able to write to data
+                        // how did these fields become public?
+                        
+                        // is it because the tests module is part of the matrix
+                        // parent module?
+
         assert_eq!(m[(0,0)]  , 0);
         assert_eq!(m._data[0], 0);
         m[(0,0)] = 1;
@@ -112,7 +180,11 @@ mod tests {
 
     #[test]
     fn one_1x1() {
-        let mut m = Matrix::new().cols(1).rows(1).fill_with(1);
+        let mut m = MatrixBuilder::new()
+            .set_cols(1)
+            .set_rows(1)
+            .finalize();
+        m.fill_with(1);
 
         assert_eq!(m.cols()  , 1 as usize);
         assert_eq!(m.rows()  , 1 as usize);
@@ -127,7 +199,11 @@ mod tests {
 
     #[test]
     fn identity_1x1() {
-        let mut m = Matrix::identity(1);
+        let mut m = MatrixBuilder::new()
+            .set_cols(1)
+            .set_rows(1)
+            .finalize();
+        m.identity();
 
         assert_eq!(m.cols()  , 1 as usize);
         assert_eq!(m.rows()  , 1 as usize);
@@ -142,7 +218,10 @@ mod tests {
 
     #[test]
     fn zero_2x2() {
-        let mut m = Matrix::fill_with(2,2,0);
+        let mut m = MatrixBuilder::new()
+            .set_cols(2)
+            .set_rows(2)
+            .finalize();
 
         assert_eq!(m.cols()  , 2 as usize);
         assert_eq!(m.rows()  , 2 as usize);
@@ -175,7 +254,11 @@ mod tests {
 
     #[test]
     fn one_2x2() {
-        let mut m = Matrix::fill_with(2,2,1);
+        let mut m = MatrixBuilder::new()
+            .set_cols(2)
+            .set_rows(2)
+            .finalize();
+        m.fill_with(1);
 
         assert_eq!(m.cols()  , 2 as usize);
         assert_eq!(m.rows()  , 2 as usize);
@@ -208,7 +291,11 @@ mod tests {
 
     #[test]
     fn identity_2x2() {
-        let mut m = Matrix::new().set_cols(2).set_rows(2).identity().finalize();
+        let mut m = MatrixBuilder::new()
+            .set_cols(2)
+            .set_rows(2)
+            .finalize();
+        m.identity();
 
         assert_eq!(m.cols()  , 2 as usize);
         assert_eq!(m.rows()  , 2 as usize);
@@ -241,10 +328,14 @@ mod tests {
 
     #[test]
     fn zero_1x2() {
-        let mut m = Matrix::new().cols(2).rows(1).fill_with(0).finalize();
+        let mut m = MatrixBuilder::new()
+            .set_cols(2)
+            .set_rows(1)
+            .finalize();
+        m.fill_with(0);
 
-        assert_eq!(m.cols() , 1 as usize);
-        assert_eq!(m.rows() , 2 as usize);
+        assert_eq!(m.cols() , 2 as usize);
+        assert_eq!(m.rows() , 1 as usize);
 
         assert_eq!(m[(0,0)] , 0);
         assert_eq!(m[(0,1)] , 0);
